9b463dd243fdb9c9719c530f547c53e736bf66fe
dinhplnguyen <dinhplnguyen@gmail.com>
2022-10-06 13:19:47 -0700

    migrate

--- Program.cs
+++ Program.cs
@@ -43,4 +43,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+  var services = scope.ServiceProvider;
+  var context = services.GetRequiredService<HealthContext>();
+  context.Database.Migrate();
+}
+
 app.Run();
